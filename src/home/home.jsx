af2fedfe25811a63881cdf5bf5db973596d967ab
Matheus Cristian <mathues@email.com>
2017-09-19 17:42:07 -0300

    Correção da home

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -2,25 +2,22 @@ import React from 'react'
 
 export default props => (
     <section className="home">
-        <div className="bgBlackHome"/>
-        <article className="textos">
-            <h1>ORGANIZE SUAS TAREFAS<br/>CONTROLE SEU DIA</h1>
-            <h2>Planeje suas atividades e seus projetos, obtenha mais tempo e torne-se mais produtivo utilizando os eficientes serviços da Tasklist</h2>
-            <div><h3>Crie sua conta gratuitamente</h3><i className='fa fa-angle-double-right'></i></div>
-        </article>
-        <article className="createAcount">
-            <h4>Cadastre-se através de outra conta</h4>
-            <form>
-                <input className="google"/>
-                <input className="facebook"/>
-            </form>
-            <span>ou</span>
-            <form onSubmit="">
-                <input placeholder="Seu nome"/>
-                <input placeholder="Seu e-mail"/>
-                <input placeholder="Sua senha"/>
-                <input className='botao' type='submit' value='Adicionar' />
-            </form>
-        </article>
+        <div className="bgHomeImg"></div>
+        <div className="content">
+            <article className="textos">
+                <h1>ORGANIZE SUAS TAREFAS<br/>CONTROLE SEU DIA</h1>
+                <h2>Planeje suas atividades e seus projetos, obtenha mais tempo e torne-se mais produtivo utilizando os eficientes serviços da Tasklist</h2>
+                <div><h3>Crie sua conta gratuitamente</h3><i className='fa fa-angle-double-right'></i></div>
+            </article>
+            <article className="createAcount">
+                <h4>Cadastre-se na Tasklist:</h4>
+                <form onSubmit="">
+                    <input placeholder="Seu nome"/>
+                    <input placeholder="Seu e-mail"/>
+                    <input placeholder="Sua senha"/>
+                    <input className='botao' type='submit' value='Criar minha conta' />
+                </form>
+            </article>
+        </div>
     </section>
 )
